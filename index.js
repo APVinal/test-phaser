b36341295fc0fd190f5b50d572c0a63ee9f6d6be
Casey Olson <casey@bookcasey.com>
2017-08-23 17:01:38 -0500

    put that rogue ! back inside the quotes!

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, function () {
-	console.log(`App listening on port ${port}`!);
+	console.log(`App listening on port ${port}!`);
 });
